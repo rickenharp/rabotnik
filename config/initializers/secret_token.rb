817e7c5612dbbbbc1f3017047310432139189237
Alexander Graefe <alex@codemo.de>
2013-05-03 15:46:17 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rabotnik::Application.config.secret_token = '61dbc5f2ff903368de08a577144a139b95bf5761b5e93a9404c37a06516b3ffb9cada3f0792495af1be1cf0fda004bc345886a8cb14f1e5bdff8254d312584bc'
